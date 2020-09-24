551be7d9926866bf9b06e2fdd2bfd42326a98590
Kevin Blount <kvbchamp@gmail.com>
2020-09-24 15:51:04 -0400

    all shoe api data shows on homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shoes: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/api/shoes/")
+      .then((response) => response.json())
+      .then((data) => this.setState({ shoes: data }));
+  }
+  render() {
+    return (
+      <div>
+        <h1>Shoes</h1>
+        <ul>
+          {this.state.shoes.map((shoe) => (
+            <li key={shoe.toString()}>
+              <p>
+                <strong>Brand Name: </strong>
+                {shoe.brand_name}
+              </p>
+              <p>
+                <strong>Shoe Color: </strong>
+                {shoe.shoe_color}
+              </p>
+              <p>
+                <strong>Fasten Type: </strong>
+                {shoe.fasten_type}
+              </p>
+              <p>
+                <strong>Manufacturer: </strong>
+                {shoe.manufacturer}
+              </p>
+              <p>
+                <strong>Material: </strong>
+                {shoe.material}
+              </p>
+              <p>
+                <strong>Shoe Type: </strong>
+                {shoe.shoe_type}
+              </p>
+              <p>
+                <strong>Size: </strong>
+                {shoe.size}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
